4bbd87aa4afa96d07d4297640fae11426424d493
Colm <colm@stratait.ie>
2023-11-01 20:26:52 +0000

    Add tests and implementation for Resource Assignment

--- ProjectManager/Class1.cs
+++ ProjectManager/Class1.cs
@@ -1,4 +1,6 @@
-﻿namespace ProjectManager;
+﻿using PrimativeExtensions;
+
+namespace ProjectManager;
 
 public class ProjectTask
 {
@@ -16,6 +18,49 @@ public class ProjectTask
     public int Id { get; private set; }
     public string Name { get; private set; }
     public string Description { get; private set; }
+    
+    public IReadOnlyCollection<RecourceRequired> ResourcesRequired { get; private set; } = new List<RecourceRequired>();
+
+    public IReadOnlyCollection<ResourceAssigned> ResourcesAssigned
+    {
+        get
+        {
+            return ResourcesRequired
+                .SelectMany(r => r.ResourcesAssigned)
+                .ToList();
+        }
+    }
+
+    public Validation<RecourceRequired> AddResourcesRequired(RecourceRequired resource)
+    {
+        if (ResourcesRequired.Any(r => r.Name.Equals(resource.Name, StringComparison.OrdinalIgnoreCase)))
+        {
+            return Validation<RecourceRequired>.Fail($"The resource '{resource.Name}' is already a requirement for this task.");
+        }
+        var resourcesRequired = ResourcesRequired.ToList();
+        resourcesRequired.Add(resource);
+        ResourcesRequired = resourcesRequired;
+        return resource; 
+    }
+
+    public Validation<ProjectTask> AssignResourceToResourceRequired(string resourceName, ResourceAssigned resourceAssigned)
+    {
+        // Check that the resource required exists
+        if (!ResourcesRequired.Any(r => r.Name.Equals(resourceName, StringComparison.OrdinalIgnoreCase)))
+        {
+            return Validation<ProjectTask>.Fail($"A resource required with the name '{resourceName}' does not exist in this task.");
+        }
+        // Get the resource required
+        var resourceRequired = ResourcesRequired.First(r => r.Name.Equals(resourceName, StringComparison.OrdinalIgnoreCase));
+        // Check that the resource is not already assigned to the resource required
+        if (resourceRequired.ResourcesAssigned.Any(r => r.Name.Equals(resourceAssigned.Name, StringComparison.OrdinalIgnoreCase)))
+        {
+            return Validation<ProjectTask>.Fail($"The resource '{resourceAssigned.Name}' is already assigned to this resource required.");
+        }
+        // Add the resource to the resource required
+        var addResult =  resourceRequired.AddResourceAssigned(resourceAssigned);
+        return addResult.Map(x => this); 
+    }
 }
 
 public class RecourceRequired
@@ -25,6 +70,28 @@ public class RecourceRequired
     public string Description { get; private set; }
     public IList<ResourceAssigned> ResourcesAssigned { get; private set; } = new List<ResourceAssigned>();
     public int Quantity { get; private set; }
+
+    public static RecourceRequired Create(string resourceName, string resourceDescription, int resourceQuantity)
+    {
+        return new RecourceRequired
+        {
+            Name = resourceName,
+            Description = resourceDescription,
+            Quantity = resourceQuantity
+        }; 
+    }
+
+    public Validation<ResourceAssigned> AddResourceAssigned(ResourceAssigned resourceAssigned)
+    {
+        if (ResourcesAssigned.Any(r => r.Name.Equals(resourceAssigned.Name, StringComparison.OrdinalIgnoreCase)))
+        {
+            return Validation<ResourceAssigned>.Fail($"The resource '{resourceAssigned.Name}' is already assigned to this resource required.");
+        }
+        var resourcesAssigned = ResourcesAssigned.ToList();
+        resourcesAssigned.Add(resourceAssigned);
+        ResourcesAssigned = resourcesAssigned;
+        return resourceAssigned;
+    }
 }
 
 public class ResourceAssigned
@@ -33,4 +100,15 @@ public class ResourceAssigned
     public string Name { get; private set; }
     public string Description { get; private set; }
     public int Quantity { get; private set; }
+
+    public static ResourceAssigned Create(string resourceAssignedName, string resourceAssignedDescription, int resourceAssignedQuantity)
+    {
+        return new ResourceAssigned
+        {
+            Name = resourceAssignedName,
+            Description = resourceAssignedDescription,
+            Quantity = resourceAssignedQuantity
+        };
+
+    }
 }
--- ProjectManager/Project.cs
+++ ProjectManager/Project.cs
@@ -28,10 +28,56 @@ public class Project
         {
             return Validation<ProjectTask>.Fail($"A task with the name '{task}' already exists in this project.");
         }
+
         var projectTask = ProjectTask.Create(task, details);
         Tasks.Add(projectTask);
         return projectTask;
     }
-    
-    
+
+    // Method to add a required resource to a task
+    public Validation<ProjectTask> AddResourceToTask(string taskName, RecourceRequired resource)
+    {
+        // Check that the task exists
+        if (!Tasks.Any(t => t.Name.Equals(taskName, StringComparison.OrdinalIgnoreCase)))
+        {
+            return Validation<ProjectTask>.Fail($"A task with the name '{taskName}' does not exist in this project.");
+        }
+
+        // Get the task
+        var task = Tasks.First(t => t.Name.Equals(taskName, StringComparison.OrdinalIgnoreCase));
+        // Check that the resource is not already assigned to the task
+        if (task.ResourcesRequired.Any(r => r.Name.Equals(resource.Name, StringComparison.OrdinalIgnoreCase)))
+        {
+            return Validation<ProjectTask>.Fail($"The resource '{resource.Name}' is already assigned to this task.");
+        }
+
+        // Add the resource to the task
+        var addResult = task.AddResourcesRequired(resource);
+        return addResult.Map(x => task);
+    }
+
+    public Validation<Project> AssignResourceToResourceRequired(string taskName, string resourceName,
+        ResourceAssigned resourceAssigned)
+    {
+        // Check that the task exists
+        if (!Tasks.Any(t => t.Name.Equals(taskName, StringComparison.OrdinalIgnoreCase)))
+        {
+            return Validation<Project>.Fail($"A task with the name '{taskName}' does not exist in this project.");
+        }
+
+        // Get the task
+        var task = Tasks.First(t => t.Name.Equals(taskName, StringComparison.OrdinalIgnoreCase));
+        // Get the resource required
+        Validation<ProjectTask> result = task.AssignResourceToResourceRequired(resourceName, resourceAssigned);
+        return result.Map(x => this);
+    }
+
+    public Validation<ProjectTask> GetTaskByName(string taskName)
+    {
+        if (!Tasks.Any(t => t.Name.Equals(taskName, StringComparison.OrdinalIgnoreCase)))
+        {
+            return Validation<ProjectTask>.Fail($"A task with the name '{taskName}' does not exist in this project.");
+        }
+        return Tasks.First(t => t.Name.Equals(taskName, StringComparison.OrdinalIgnoreCase));
+    }
 }
