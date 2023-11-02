afac1904f09ef1fc52e2d8c7f6d09674caaba401
Colm <colm@stratait.ie>
2023-11-02 11:02:40 +0000

    Rename ValidationExtensions file from class1

--- PrimativeExtensions/Validation.cs
+++ PrimativeExtensions/Validation.cs
@@ -141,3 +141,39 @@ public class Validation<T>
     
     
 }
+
+public static class Validation
+{
+    public static TR OnValid<T, U, TR>( Validation<T> validation, Validation<U> validation2, Func<T, U, TR> onValid, Func<Validation<T>, TR> onInvalid, Func<Validation<U>, TR> onInvalid2)
+    {
+        if (validation.IsFailure)
+        {
+            return onInvalid(validation);
+        }
+
+        if (validation2.IsFailure)
+        {
+            return onInvalid2(validation2);
+        }
+
+        return onValid(validation.Value, validation2.Value);
+    }
+    
+    public static void OnValid<T, U>( Validation<T> validation, Validation<U> validation2, Action<T, U> onValid, Action<Validation<T>> onInvalid, Action<Validation<U>> onInvalid2)
+    {
+        if (validation.IsFailure)
+        {
+            onInvalid(validation);
+            return;
+        }
+
+        if (validation2.IsFailure)
+        {
+            onInvalid2(validation2);
+            return;
+        }
+
+        onValid(validation.Value, validation2.Value);
+    }
+
+}
--- PrimativeExtensions/ValidationExtensions.cs
+++ PrimativeExtensions/ValidationExtensions.cs
@@ -1,40 +1,6 @@
 ﻿namespace PrimativeExtensions;
 
-public static class Validation
-{
-    public static TR OnValid<T, U, TR>( Validation<T> validation, Validation<U> validation2, Func<T, U, TR> onValid, Func<Validation<T>, TR> onInvalid, Func<Validation<U>, TR> onInvalid2)
-    {
-        if (validation.IsFailure)
-        {
-            return onInvalid(validation);
-        }
 
-        if (validation2.IsFailure)
-        {
-            return onInvalid2(validation2);
-        }
-
-        return onValid(validation.Value, validation2.Value);
-    }
-    
-    public static void OnValid<T, U>( Validation<T> validation, Validation<U> validation2, Action<T, U> onValid, Action<Validation<T>> onInvalid, Action<Validation<U>> onInvalid2)
-    {
-        if (validation.IsFailure)
-        {
-            onInvalid(validation);
-            return;
-        }
-
-        if (validation2.IsFailure)
-        {
-             onInvalid2(validation2);
-             return;
-        }
-
-        onValid(validation.Value, validation2.Value);
-    }
-
-}
 public static class ValidationExtensions
 {
     public static IEnumerable<T> AsEnumerable<T>(this Validation<IEnumerable<T>> validation)
